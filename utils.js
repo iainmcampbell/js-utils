cef3cbf6882f279ac8b6bb94181f78103e1eebb6
iainmcampbell <hey@iainmc.ca>
2017-07-13 13:11:37 -0400

    more functions

--- utils.js
+++ utils.js
@@ -50,6 +50,19 @@ module.exports = {
     return parseFloat( number.toFixed(precision) )
   },
 
+  normalizeDegrees: function(deg){
+    if(deg > 360){
+      return 360 - (deg % 360)
+    } else if(deg < 0){
+      return 360 + (deg % 360)
+    } else {
+      return deg
+    }
+  },
+
+ radToDeg: function(rad){ return 360 * (rad / (Math.PI*2)) },
+ degToRad: function(deg){ return (deg / 360) * (Math.PI*2) },
+
 
 
   /*
@@ -62,15 +75,59 @@ module.exports = {
 
   */
 
+  stripUrls: function (string){
+    return string.replace(/(http|https):\S*/g, '')
+  },
+
+  // global find and replace
+  replaceAll: function(string, search, replacement){
+    return string.replace(new RegExp(search, 'g'), replacement)
+  },
+
+  pad: function(number, width, zeroCharacter) {
+    zeroCharacter = zeroCharacter || '0';
+    number = number + '';
+    return number.length >= width ? number : new Array(width - number.length + 1).join(zeroCharacter) + number;
+  },
+
+
+  /*
+
+     #####  #####  #####   ##### ##    ## ####
+    ##   ## ##  ## ##  ## ##   ## ##  ## ##
+    ####### #####  #####  #######  ####   ####
+    ##   ## ##  ## ##  ## ##   ##   ##       ##
+    ##   ## ##  ## ##  ## ##   ##   ##   #####
+
+  */
+
+
+  // Reorders array values randomly.
+  shuffleArray: function(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  },
+
   // remove string from array
   remove: function (array, string){
     var i = array.indexOf(string)
     if( i !== -1 ) array.splice(i,1)
   },
 
-  stripUrls: function (string){
-    return string.replace(/(http|https):\S*/g, '')
-  },
 
 
 
@@ -87,8 +144,15 @@ module.exports = {
   loadImg: function (src){
     return new Promise(function(resolve, reject){
       var img = new Image()
-      img.onload = function(){ resolve(img) }
+      img.onload = resolve.bind(null, img)
+      img.onnerror = reject.bind(img)
       img.src = src
+    })
+  },
+
+  timeoutPromise: function(duration){
+    return new Promise(function(resolve){
+      setTimeout(resolve, duration)
     })
   },
 
@@ -105,6 +169,74 @@ module.exports = {
         }
 
     return output
+  },
+
+  /*
+
+    shallow object diff:
+    returns true if objects have changed, false if objects are identical
+
+  */
+  diff: function(a, b){
+    var aKeys = Object.keys(a)
+    var bKeys = Object.keys(b)
+
+    if(aKeys.length !== bKeys.length){
+      return true
+    }
+
+    for (var i = aKeys.length - 1; i >= 0; i--) {
+      if(bKeys.indexOf(aKeys[i]) === -1)
+        return true
+      else if( a[aKeys[i]] !== b[aKeys[i]] )
+        return true
+    }
+
+    return false
+  },
+
+  /*
+
+    helper function for Mithril.js
+
+    computeClass({
+      'is-visible': true,
+      'is-active':  true,
+    })
+
+    returns 'is-visible is-active'
+
+  */
+  computeClass: function(obj){
+    var className = ''
+    Object.keys(obj).forEach(function(key){
+      if(obj[key])
+        className += ' '+ key
+    })
+    return className
+  },
+
+  /*
+
+    very simple templating:
+
+    var result = varstring('{one}, {two}', { one: 1, two: 2 })
+    -> '1, 2'
+
+  */
+  varstring: function(string, params) {
+    if(params instanceof Object) {
+      //do nothing
+    }
+    else if(!(params instanceof Array)) {
+      params = Array.prototype.slice.call(arguments)
+      string = params.shift()
+    }
+
+    Object.keys(params).forEach(function(v) {
+      string = string.replace(new RegExp('{'+v+'}','g'),params[v])
+    })
+    return string
   },
 
 
