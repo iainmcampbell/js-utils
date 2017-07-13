bb79ba382369947528baed2618734618ffd925ad
iainmcampbell <hey@iainmc.ca>
2017-07-13 12:34:28 -0400

    i am a smart man

--- utils.js
+++ utils.js
@@ -13,27 +13,27 @@ module.exports = {
   rand: function (low, high){
     var u = Math.random();
     return (1-u)*low + u*high;
-  }
+  },
 
   mapRange: function (value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
-  }
+  },
 
   // randomize true/false
   randBool: function (){
     return (Math.random()<0.5)
-  }
+  },
 
   // randomize positive/negative status of a number
   // ie given 2, will return either -2 or 2
   posneg: function (num){
     return (Math.random()<0.5) ? num : -num
-  }
+  },
 
   // linear interpolation
   lerp: function (start, end, now) {
     return (1 - now) * start + now * end;
-  }
+  },
 
   // limit if limit is less than num
   least: function (){
@@ -44,11 +44,11 @@ module.exports = {
         l = arguments[i]
     }
     return l
-  }
+  },
 
   precision: function (number, precision){
     return parseFloat( number.toFixed(precision) )
-  }
+  },
 
 
 
@@ -66,11 +66,11 @@ module.exports = {
   remove: function (array, string){
     var i = array.indexOf(string)
     if( i !== -1 ) array.splice(i,1)
-  }
+  },
 
   stripUrls: function (string){
     return string.replace(/(http|https):\S*/g, '')
-  }
+  },
 
 
 
@@ -90,7 +90,7 @@ module.exports = {
       img.onload = function(){ resolve(img) }
       img.src = src
     })
-  }
+  },
 
   // shallow object extend
   extend: function (){
@@ -105,7 +105,7 @@ module.exports = {
         }
 
     return output
-  }
+  },
 
 
 }
