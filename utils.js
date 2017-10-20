5dbe687c5986eddc6f9c03f46aa875557cd4ac14
iainmcampbell <hey@iainmc.ca>
2017-10-20 16:26:25 -0400

    check scrollbar width

--- utils.js
+++ utils.js
@@ -256,5 +256,21 @@ module.exports = {
     return (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0)
   },
 
+  checkScrollbarWidth: function(){
+    var el = document.createElement('div')
+
+    el.style.width  = '100px'
+    el.style.height = '100px'
+    el.style.overflow = 'scroll'
+    el.style.position = 'absolute'
+    el.style.top = '-9999px'
+
+    document.body.appendChild(el)
+
+    var scrollbarWidth = el.offsetWidth - el.clientWidth
+    document.body.removeChild(el)
+
+    return scrollbarWidth
+  },
 
 }
